34473230225de18964c77e22f279c30540719ea7
Willy <willyzha@gmail.com>
2018-02-23 11:24:49 -0500

    Initial protopype for Network detection sensor
    Detects if device is connected to network

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var Service, Characteristic;
 
+var sys = require('sys')
+var exec = require('child_process').exec;
+
 module.exports = function(homebridge) {
   console.log("homebridge API version: " + homebridge.version);
 
@@ -18,16 +21,47 @@ function NetworkSensorAccessory(log, config) {
   this.name = config["name"];
   this.mac = config["mac"];
   this.updateInterval = config["update_interval"];
+  this.detection_timeout = config["detection_timeout"];
+  if (this.updateInterval > this.detection_timeout)
+  {
+      this.detection_timeout = this.updateInterval;
+  }
+  this.detectedOccupancyValue = false
+  this.outputOccupancyValue = false
 }
 
+//function puts(error, stdout, stderr) { sys.puts(stdout) }
+
 NetworkSensorAccessory.prototype = {
-  updateState: function () {
-    var newOccupancyValue = True;
-    // CODE TO CHECK IF MAC ADDRESS IS ON NETWORK
-    this.occupancyService.getCharacteristic(Characteristic.OccupancyDetected)
-                          .setValue(newOccupancyValue ? Characteristic.OccupancyDetected.OCCUPANCY_DETECTED : Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED);
-  },
   
+  puts: function(error, stdout, stderr) { 
+    console.log(stdout)
+    if (!stdout) {
+      this.detectedOccupancyValue = false
+      console.log(this.detectedOccupancyValue)
+    } else {
+      this.detectedOccupancyValue = true
+      console.log(this.detectedOccupancyValue)
+    }
+
+    this.outputOccupancyValue = this.detectedOccupancyValue;
+    
+    this.occupancyService.getCharacteristic(Characteristic.OccupancyDetected)
+                          .setValue(this.outputOccupancyValue ? Characteristic.OccupancyDetected.OCCUPANCY_DETECTED : Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED);
+    this.log("Detected: " + this.detectedOccupancyValue + " Output: " + this.outputOccupancyValue)
+  },
+
+  updateState: function () {
+    // CODE TO CHECK IF MAC ADDRESS IS ON NETWORK
+
+    exec("sudo arp-scan --interface=wlan0 -l| grep -i " + this.mac, this.puts.bind(this));
+
+    //this.log(this.detectedOccupancyValue )
+
+    //this.occupancyService.getCharacteristic(Characteristic.OccupancyDetected)
+    //                      .setValue(this.detectedOccupancyValue ? Characteristic.OccupancyDetected.OCCUPANCY_DETECTED : Characteristic.OccupancyDetected.OCCUPANCY_NOT_DETECTED);
+  },
+
   getServices: function() {
 
     // you can OPTIONALLY create an information service if you wish to override
